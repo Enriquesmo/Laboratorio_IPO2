1818cc0c4082eb641cfa755fbb9f06b2f38176bf
Enrique Sánchez-Migallón Ochoa <Enrique.Sanchez9@alu.uclm.es>
2023-02-24 01:20:06 +0100

    Sesión 3 - Versión 3.2 - Versión Estable, todo preparado para inciar con la programación y creación de las animaciones.

--- Laboratorio_IPO2/MainPage.xaml.cs
+++ Laboratorio_IPO2/MainPage.xaml.cs
@@ -19,19 +19,27 @@ using Windows.UI.Xaml.Navigation;
 namespace Laboratorio_IPO2
 {
     /// <summary>
-    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// Página dedicada al primer trabajo del laboratorio de IPO2
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        DispatcherTimer dtTime;
         /************************************************************************************************/
 
-        /*Inicializacion de la ventana MainWindow*/
+        /*Inicializacion de las variables globales*/
+
+        DispatcherTimer dtTimeVida;
+        DispatcherTimer dtTimeEnergia;
+
+        /************************************************************************************************/
+
+        /*Inicializacion de la pagina MainPage*/
 
         public MainPage()
         {
             this.InitializeComponent();
-            dtTime = new DispatcherTimer();
+            dtTimeVida = new DispatcherTimer();
+            dtTimeEnergia = new DispatcherTimer();
+
             /*Storyboard sb = (Storyboard)this.Resources["ManitaDerecha"];
             sb.AutoReverse = true;
             sb.RepeatBehavior = RepeatBehavior.Forever;
@@ -43,32 +51,90 @@ namespace Laboratorio_IPO2
             sb2.Begin();
             */
         }
+
         /************************************************************************************************/
 
         /*Botones de la propia ventana ListaDeRutas*/
 
         private void Image_Pocion_De_Vida_PointerReleased(object sender, PointerRoutedEventArgs e)
         {
-            
-            dtTime = new DispatcherTimer();
-            dtTime.Interval = TimeSpan.FromMilliseconds(10); // Esto lo he cambiado para que se vea más fluido, deberia ser 100 milisegundos
-            dtTime.Tick += increaseHealth;
-            dtTime.Start();
+            dtTimeVida = new DispatcherTimer();
+            dtTimeVida.Interval = TimeSpan.FromMilliseconds(10); // Esto lo he cambiado para que se vea más fluido, deberia ser 100 milisegundos
+            dtTimeVida.Tick += increaseHealth;
+            dtTimeVida.Start();
             this.Image_Pocion_De_Vida.Opacity = 0.5;
-            
         }
+
+        private void Image_Pocion_De_Energia_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            dtTimeEnergia = new DispatcherTimer();
+            dtTimeEnergia.Interval = TimeSpan.FromMilliseconds(10); // Esto lo he cambiado para que se vea más fluido, deberia ser 100 milisegundos
+            dtTimeEnergia.Tick += increaseEnergy;
+            dtTimeEnergia.Start();
+            this.Image_Pocion_De_Energia.Opacity = 0.5;
+        }
+
+        private void btn_PrimerAtaque_Click(object sender, RoutedEventArgs e)
+        {
+            restarBarraEnergia(20);
+        }
+
+        private void btn_SegundoAtaque_Click(object sender, RoutedEventArgs e)
+        {
+            restarBarraEnergia(10);
+        }
+
+        private void btn_TercerAtaque_Click(object sender, RoutedEventArgs e)
+        {
+            restarBarraEnergia(25);
+        }
+
+        private void btn_CuartoAtaque_Click(object sender, RoutedEventArgs e)
+        {
+            restarBarraEnergia(40);
+        }
+
+        private void btn_Herir_Click(object sender, RoutedEventArgs e)
+        {
+            restarBarraVida(25);
+        }
+
         /************************************************************************************************/
 
-        /*Metodos Auxiliares para todos los botones*/
+        /*Metodos Auxiliares para los botones e imagenes*/
 
         private void increaseHealth(object sender, object e)
         {
            this.ProgressBar_Vida.Value += 0.2;
            if (ProgressBar_Vida.Value >= 100)
            {
-                this.dtTime.Stop();
+                this.dtTimeVida.Stop();
                 this.Image_Pocion_De_Vida.Opacity = 1;
            }
+        }
+
+        private void increaseEnergy(object sender, object e)
+        {
+            this.ProgressBar_Energia.Value += 0.2;
+            if (ProgressBar_Energia.Value >= 100)
+            {
+                this.dtTimeEnergia.Stop();
+                this.Image_Pocion_De_Energia.Opacity = 1;
+            }
+        }
+
+        private void restarBarraEnergia(int valorARestar)
+        {
+            this.ProgressBar_Energia.Value -= valorARestar;
+            dtTimeEnergia.Stop();
+            this.Image_Pocion_De_Energia.Opacity = 1;
+        }
+
+        private void restarBarraVida(int valorARestar)
+        {
+            this.ProgressBar_Vida.Value -= valorARestar;
+            dtTimeVida.Stop();
+            this.Image_Pocion_De_Vida.Opacity = 1;
         }
     }
 }
