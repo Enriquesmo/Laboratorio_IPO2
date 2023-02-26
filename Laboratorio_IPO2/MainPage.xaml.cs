c02a769465f4f9ce110c20b9d75eb7de941544ac
Enrique Sánchez-Migallón Ochoa <Enrique.Sanchez9@alu.uclm.es>
2023-02-26 20:35:16 +0100

    Sesión 3 - Versión 3.3 - Estado de Muerte completado. Falta terminar de animar los ataques 1,2 y 3. El 3 le falta buscar la imagen. El ataque 4 no hay ningún plan para el mismo. Buscar a ver si se pueden hacer más animaciones.

--- Laboratorio_IPO2/MainPage.xaml.cs
+++ Laboratorio_IPO2/MainPage.xaml.cs
@@ -39,6 +39,43 @@ namespace Laboratorio_IPO2
             this.InitializeComponent();
             dtTimeVida = new DispatcherTimer();
             dtTimeEnergia = new DispatcherTimer();
+            movimientoEstandar();
+            ocultarElementosExtras();
+        }
+
+        /************************************************************************************************/
+
+        /*Botones de la propia ventana ListaDeRutas*/
+
+        private void Image_Pocion_De_Vida_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (ProgressBar_Vida.Value > 0)
+            {
+                dtTimeVida = new DispatcherTimer();
+                dtTimeVida.Interval = TimeSpan.FromMilliseconds(10); // Esto lo he cambiado para que se vea más fluido, deberia ser 100 milisegundos
+                dtTimeVida.Tick += increaseHealth;
+                dtTimeVida.Start();
+                this.Image_Pocion_De_Vida.Opacity = 0.5;
+            }
+        }
+        private void Image_Pocion_De_Energia_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (ProgressBar_Vida.Value > 0)
+            {
+                dtTimeEnergia = new DispatcherTimer();
+                dtTimeEnergia.Interval = TimeSpan.FromMilliseconds(10); // Esto lo he cambiado para que se vea más fluido, deberia ser 100 milisegundos
+                dtTimeEnergia.Tick += increaseEnergy;
+                dtTimeEnergia.Start();
+                this.Image_Pocion_De_Energia.Opacity = 0.5;
+            }
+        }
+        private void btn_PrimerAtaque_Click(object sender, RoutedEventArgs e)
+        {
+            restarBarraEnergia(20);
+            Storyboard sb = (Storyboard)this.Resources["Ataque1_Arañazo"];
+            sb.Begin();
+            Ataque1_ImagenArañazo.Visibility = Visibility.Visible;
+            movimientoEstandar();
 
             /*Storyboard sb = (Storyboard)this.Resources["ManitaDerecha"];
             sb.AutoReverse = true;
@@ -51,58 +88,51 @@ namespace Laboratorio_IPO2
             sb2.Begin();
             */
         }
-
-        /************************************************************************************************/
-
-        /*Botones de la propia ventana ListaDeRutas*/
-
-        private void Image_Pocion_De_Vida_PointerReleased(object sender, PointerRoutedEventArgs e)
-        {
-            dtTimeVida = new DispatcherTimer();
-            dtTimeVida.Interval = TimeSpan.FromMilliseconds(10); // Esto lo he cambiado para que se vea más fluido, deberia ser 100 milisegundos
-            dtTimeVida.Tick += increaseHealth;
-            dtTimeVida.Start();
-            this.Image_Pocion_De_Vida.Opacity = 0.5;
-        }
-
-        private void Image_Pocion_De_Energia_PointerReleased(object sender, PointerRoutedEventArgs e)
-        {
-            dtTimeEnergia = new DispatcherTimer();
-            dtTimeEnergia.Interval = TimeSpan.FromMilliseconds(10); // Esto lo he cambiado para que se vea más fluido, deberia ser 100 milisegundos
-            dtTimeEnergia.Tick += increaseEnergy;
-            dtTimeEnergia.Start();
-            this.Image_Pocion_De_Energia.Opacity = 0.5;
-        }
-
-        private void btn_PrimerAtaque_Click(object sender, RoutedEventArgs e)
-        {
-            restarBarraEnergia(20);
-        }
-
         private void btn_SegundoAtaque_Click(object sender, RoutedEventArgs e)
         {
             restarBarraEnergia(10);
+            Storyboard sb = (Storyboard)this.Resources["Ataque2_AtaqueArena"];
+            sb.Begin();
+            movimientoEstandar();
         }
-
         private void btn_TercerAtaque_Click(object sender, RoutedEventArgs e)
         {
             restarBarraEnergia(25);
+            Storyboard sb = (Storyboard)this.Resources["Ataque3_GolpeCabeza"];
+            sb.Begin();
+            movimientoEstandar();
         }
-
         private void btn_CuartoAtaque_Click(object sender, RoutedEventArgs e)
         {
             restarBarraEnergia(40);
+            Storyboard sb = (Storyboard)this.Resources["Ataque4_Terremoto"];
+            sb.Begin();
+            movimientoEstandar();
         }
-
         private void btn_Herir_Click(object sender, RoutedEventArgs e)
         {
             restarBarraVida(25);
+            if (this.ProgressBar_Vida.Value <= 50 && this.ProgressBar_Vida.Value > 25)
+            {
+                Vida50.Visibility = Visibility.Visible;
+            } else if (this.ProgressBar_Vida.Value <= 25 && this.ProgressBar_Vida.Value > 0)
+            {
+                Vida25.Visibility = Visibility.Visible;
+            } else if (this.ProgressBar_Vida.Value <= 0)
+            {
+                animacionVida0();
+            }
         }
 
         /************************************************************************************************/
 
         /*Metodos Auxiliares para los botones e imagenes*/
 
+        private void movimientoEstandar()
+        {
+            Storyboard sb = (Storyboard)this.Resources["MovimientoEstandar"];
+            sb.Begin();
+        }
         private void increaseHealth(object sender, object e)
         {
            this.ProgressBar_Vida.Value += 0.2;
@@ -112,7 +142,6 @@ namespace Laboratorio_IPO2
                 this.Image_Pocion_De_Vida.Opacity = 1;
            }
         }
-
         private void increaseEnergy(object sender, object e)
         {
             this.ProgressBar_Energia.Value += 0.2;
@@ -122,19 +151,44 @@ namespace Laboratorio_IPO2
                 this.Image_Pocion_De_Energia.Opacity = 1;
             }
         }
-
         private void restarBarraEnergia(int valorARestar)
         {
-            this.ProgressBar_Energia.Value -= valorARestar;
-            dtTimeEnergia.Stop();
-            this.Image_Pocion_De_Energia.Opacity = 1;
+            if (ProgressBar_Energia.Value >= valorARestar)
+            {
+                this.ProgressBar_Energia.Value -= valorARestar;
+                dtTimeEnergia.Stop();
+                this.Image_Pocion_De_Energia.Opacity = 1;
+            }
         }
-
         private void restarBarraVida(int valorARestar)
         {
             this.ProgressBar_Vida.Value -= valorARestar;
             dtTimeVida.Stop();
             this.Image_Pocion_De_Vida.Opacity = 1;
+        }
+        private void animacionVida0()
+        {
+            Vida0.Visibility = Visibility.Visible;
+            Ojo_Derecho.Visibility = Visibility.Collapsed;
+            Ojo_Izquierdo.Visibility = Visibility.Collapsed;
+            Storyboard sb = (Storyboard)this.Resources["vida0"];
+            sb.Begin();
+            estadoBotones(false);
+        }
+        private void ocultarElementosExtras()
+        {
+            Vida50.Visibility = Visibility.Collapsed;
+            Vida25.Visibility = Visibility.Collapsed;
+            Vida0.Visibility = Visibility.Collapsed;
+            Ataque1_ImagenArañazo.Visibility = Visibility.Collapsed;
+        }
+        private void estadoBotones(Boolean estado)
+        {
+            btn_PrimerAtaque.IsEnabled = estado;
+            btn_SegundoAtaque.IsEnabled = estado;
+            btn_TercerAtaque.IsEnabled = estado;
+            btn_CuartoAtaque.IsEnabled = estado;
+            btn_Herir.IsEnabled = estado;
         }
     }
 }
