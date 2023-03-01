484c3574243b411039674746a3295b400a32a25e
Enrique Sánchez-Migallón Ochoa <Enrique.Sanchez9@alu.uclm.es>
2023-03-01 10:07:13 +0100

    Sesión 3 - Versión 3.4 - Animaciones de Ataques completadas y estables. Falta añadir animacion de no tener energía y la animacion estática o por Defecto.

--- Laboratorio_IPO2/MainPage.xaml.cs
+++ Laboratorio_IPO2/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,6 +14,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using static System.Net.Mime.MediaTypeNames;
+using Image = Windows.UI.Xaml.Controls.Image;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
 
@@ -34,7 +37,7 @@ namespace Laboratorio_IPO2
 
         /*Inicializacion de la pagina MainPage*/
 
-        public MainPage()
+        public MainPage() // Terminado
         {
             this.InitializeComponent();
             dtTimeVida = new DispatcherTimer();
@@ -47,7 +50,7 @@ namespace Laboratorio_IPO2
 
         /*Botones de la propia ventana ListaDeRutas*/
 
-        private void Image_Pocion_De_Vida_PointerReleased(object sender, PointerRoutedEventArgs e)
+        private void Image_Pocion_De_Vida_PointerReleased(object sender, PointerRoutedEventArgs e) // Terminado
         {
             if (ProgressBar_Vida.Value > 0)
             {
@@ -58,7 +61,7 @@ namespace Laboratorio_IPO2
                 this.Image_Pocion_De_Vida.Opacity = 0.5;
             }
         }
-        private void Image_Pocion_De_Energia_PointerReleased(object sender, PointerRoutedEventArgs e)
+        private void Image_Pocion_De_Energia_PointerReleased(object sender, PointerRoutedEventArgs e) // Terminado
         {
             if (ProgressBar_Vida.Value > 0)
             {
@@ -69,58 +72,100 @@ namespace Laboratorio_IPO2
                 this.Image_Pocion_De_Energia.Opacity = 0.5;
             }
         }
-        private void btn_PrimerAtaque_Click(object sender, RoutedEventArgs e)
+        private void Btn_PrimerAtaque_Click(object sender, RoutedEventArgs e) // Terminado
         {
-            restarBarraEnergia(20);
-            Storyboard sb = (Storyboard)this.Resources["Ataque1_Arañazo"];
-            sb.Begin();
-            Ataque1_ImagenArañazo.Visibility = Visibility.Visible;
-            movimientoEstandar();
-
-            /*Storyboard sb = (Storyboard)this.Resources["ManitaDerecha"];
-            sb.AutoReverse = true;
-            sb.RepeatBehavior = RepeatBehavior.Forever;
-            sb.Begin();
-
-            Storyboard sb2 = (Storyboard)this.Resources["Salto"];
-            sb2.AutoReverse = true;
-            sb2.RepeatBehavior = RepeatBehavior.Forever;
-            sb2.Begin();
-            */
+            Boolean sePuede = restarBarraEnergia(20);
+            if (sePuede == true)
+            {
+                Storyboard sb = (Storyboard)this.Resources["Ataque1_Arañazo"];
+                sb.Begin();
+                Ataque1_ImagenArañazo.Visibility = Visibility.Visible;
+                movimientoEstandar();
+            }
+            else
+            {
+                Storyboard sb = (Storyboard)this.Resources["NoEnergía"];
+                sb.Begin();
+            }
         }
-        private void btn_SegundoAtaque_Click(object sender, RoutedEventArgs e)
+        private void btn_SegundoAtaque_Click(object sender, RoutedEventArgs e) // Terminado
         {
-            restarBarraEnergia(10);
-            Storyboard sb = (Storyboard)this.Resources["Ataque2_AtaqueArena"];
-            sb.Begin();
-            movimientoEstandar();
+            Boolean sePuede = restarBarraEnergia(10);
+            if (sePuede == true)
+            {
+                Storyboard sb = (Storyboard)this.Resources["Ataque2_AtaqueArena"];
+                Ataque2_Arena1.Visibility = Visibility.Visible;
+                Ataque2_Arena2.Visibility = Visibility.Visible;
+                Ataque2_Arena3.Visibility = Visibility.Visible;
+                Ataque2_Arena4.Visibility = Visibility.Visible;
+                sb.Begin();
+                movimientoEstandar();
+            }
+            else
+            {
+                Storyboard sb = (Storyboard)this.Resources["NoEnergía"];
+                sb.Begin();
+            }
         }
-        private void btn_TercerAtaque_Click(object sender, RoutedEventArgs e)
+        private void btn_TercerAtaque_Click(object sender, RoutedEventArgs e) // Terminado
         {
-            restarBarraEnergia(25);
-            Storyboard sb = (Storyboard)this.Resources["Ataque3_GolpeCabeza"];
-            sb.Begin();
-            movimientoEstandar();
+            Boolean sePuede = restarBarraEnergia(25);
+            if (sePuede == true)
+            {
+                Ataque3_FlechaVenenosa.Visibility = Visibility.Visible;
+                Ataque3_BurbujasVeneno.Visibility = Visibility.Visible;
+                Storyboard sb = (Storyboard)this.Resources["Ataque3_PicotazoVen"];
+                sb.Begin();
+                Storyboard sbIzquierdo = (Storyboard)this.Ellipse_Ojo_Izquierdo_Blanco.Resources["ojoIzquierdoMoradoKey"];
+                sbIzquierdo.Begin();
+                Storyboard sbDerecho = (Storyboard)this.Ellipse_Ojo_Derecho_Blanco.Resources["ojoDerechoMoradoKey"];
+                sbDerecho.Begin();
+                movimientoEstandar();
+            }
+            else
+            {
+                Storyboard sb = (Storyboard)this.Resources["NoEnergía"];
+                sb.Begin();
+            }
         }
-        private void btn_CuartoAtaque_Click(object sender, RoutedEventArgs e)
+        private void btn_CuartoAtaque_Click(object sender, RoutedEventArgs e) // Terminado
         {
-            restarBarraEnergia(40);
-            Storyboard sb = (Storyboard)this.Resources["Ataque4_Terremoto"];
-            sb.Begin();
-            movimientoEstandar();
+            Boolean sePuede = restarBarraEnergia(40);
+            if (sePuede == true)
+            {
+                Storyboard sb = (Storyboard)this.Resources["Ataque4_Terremoto"];
+                sb.Begin();
+                Ataque4_Grieta1.Visibility = Visibility.Visible;
+                Ataque4_Grieta2.Visibility = Visibility.Visible;
+                Ataque4_Grieta3.Visibility = Visibility.Visible;
+                Ataque4_Grieta4.Visibility = Visibility.Visible;
+                Ataque4_Grieta5.Visibility = Visibility.Visible;
+                movimientoEstandar();
+            }
+            else
+            {
+                Storyboard sb = (Storyboard)this.Resources["NoEnergía"];
+                sb.Begin();
+            }
         }
-        private void btn_Herir_Click(object sender, RoutedEventArgs e)
+        private void btn_Herir_Click(object sender, RoutedEventArgs e) // Terminado
         {
             restarBarraVida(25);
             if (this.ProgressBar_Vida.Value <= 50 && this.ProgressBar_Vida.Value > 25)
             {
-                Vida50.Visibility = Visibility.Visible;
+                Vida50_Tirita1.Visibility = Visibility.Visible;
+                Vida50_Tirita2.Visibility = Visibility.Visible;
+                Vida50_Tirita3.Visibility = Visibility.Visible;
             } else if (this.ProgressBar_Vida.Value <= 25 && this.ProgressBar_Vida.Value > 0)
             {
-                Vida25.Visibility = Visibility.Visible;
+                Vida25_Arañazo1.Visibility = Visibility.Visible;
+                Vida25_Arañazo2.Visibility = Visibility.Visible;
+                Vida25_Arañazo3.Visibility = Visibility.Visible;
+                Vida25_Arañazo4.Visibility = Visibility.Visible;
             } else if (this.ProgressBar_Vida.Value <= 0)
             {
                 animacionVida0();
+                opacityElementosExtras();
             }
         }
 
@@ -130,19 +175,33 @@ namespace Laboratorio_IPO2
 
         private void movimientoEstandar()
         {
+            opacityElementosExtras();
             Storyboard sb = (Storyboard)this.Resources["MovimientoEstandar"];
             sb.Begin();
         }
-        private void increaseHealth(object sender, object e)
+        private void increaseHealth(object sender, object e) // Terminado
         {
            this.ProgressBar_Vida.Value += 0.2;
+            if (ProgressBar_Vida.Value >= 25)
+            {
+                Vida25_Arañazo1.Visibility = Visibility.Collapsed;
+                Vida25_Arañazo2.Visibility = Visibility.Collapsed;
+                Vida25_Arañazo3.Visibility = Visibility.Collapsed;
+                Vida25_Arañazo4.Visibility = Visibility.Collapsed;
+            }
+            if(ProgressBar_Vida.Value >= 50)
+            {
+                Vida50_Tirita1.Visibility = Visibility.Collapsed;
+                Vida50_Tirita2.Visibility = Visibility.Collapsed;
+                Vida50_Tirita3.Visibility = Visibility.Collapsed;
+            }
            if (ProgressBar_Vida.Value >= 100)
            {
                 this.dtTimeVida.Stop();
                 this.Image_Pocion_De_Vida.Opacity = 1;
            }
         }
-        private void increaseEnergy(object sender, object e)
+        private void increaseEnergy(object sender, object e) // Terminado
         {
             this.ProgressBar_Energia.Value += 0.2;
             if (ProgressBar_Energia.Value >= 100)
@@ -151,38 +210,75 @@ namespace Laboratorio_IPO2
                 this.Image_Pocion_De_Energia.Opacity = 1;
             }
         }
-        private void restarBarraEnergia(int valorARestar)
+        private Boolean restarBarraEnergia(int valorARestar) // Terminado
         {
+            Boolean sePuede = false;
             if (ProgressBar_Energia.Value >= valorARestar)
             {
                 this.ProgressBar_Energia.Value -= valorARestar;
                 dtTimeEnergia.Stop();
                 this.Image_Pocion_De_Energia.Opacity = 1;
+                sePuede= true;
             }
+            return sePuede;
         }
-        private void restarBarraVida(int valorARestar)
+        private void restarBarraVida(int valorARestar) // Terminado
         {
             this.ProgressBar_Vida.Value -= valorARestar;
             dtTimeVida.Stop();
             this.Image_Pocion_De_Vida.Opacity = 1;
         }
-        private void animacionVida0()
+        private void animacionVida0() // Terminado
         {
-            Vida0.Visibility = Visibility.Visible;
+            Vida0_OjoDerecho.Visibility = Visibility.Visible;
+            Vida0_OjoIzquierdo.Visibility = Visibility.Visible;
             Ojo_Derecho.Visibility = Visibility.Collapsed;
             Ojo_Izquierdo.Visibility = Visibility.Collapsed;
             Storyboard sb = (Storyboard)this.Resources["vida0"];
             sb.Begin();
             estadoBotones(false);
+            //ocultarElementosExtras();
         }
-        private void ocultarElementosExtras()
+        private void ocultarElementosExtras() // Terminado
         {
-            Vida50.Visibility = Visibility.Collapsed;
-            Vida25.Visibility = Visibility.Collapsed;
-            Vida0.Visibility = Visibility.Collapsed;
+            Vida50_Tirita1.Visibility= Visibility.Collapsed;
+            Vida50_Tirita2.Visibility = Visibility.Collapsed;
+            Vida50_Tirita3.Visibility = Visibility.Collapsed;
+            Vida25_Arañazo1.Visibility = Visibility.Collapsed;
+            Vida25_Arañazo2.Visibility = Visibility.Collapsed;
+            Vida25_Arañazo3.Visibility = Visibility.Collapsed;
+            Vida25_Arañazo4.Visibility = Visibility.Collapsed;
+            Vida0_OjoDerecho.Visibility = Visibility.Collapsed;
+            Vida0_OjoIzquierdo.Visibility= Visibility.Collapsed;
             Ataque1_ImagenArañazo.Visibility = Visibility.Collapsed;
+            Ataque2_Arena1.Visibility = Visibility.Collapsed;
+            Ataque2_Arena2.Visibility = Visibility.Collapsed;
+            Ataque2_Arena3.Visibility = Visibility.Collapsed;
+            Ataque2_Arena4.Visibility = Visibility.Collapsed;
+            Ataque3_FlechaVenenosa.Visibility = Visibility.Collapsed;
+            Ataque3_BurbujasVeneno.Visibility = Visibility.Collapsed;
+            Ataque4_Grieta1.Visibility = Visibility.Collapsed;
+            Ataque4_Grieta2.Visibility = Visibility.Collapsed;
+            Ataque4_Grieta3.Visibility = Visibility.Collapsed;
+            Ataque4_Grieta4.Visibility = Visibility.Collapsed;
+            Ataque4_Grieta5.Visibility = Visibility.Collapsed;
         }
-        private void estadoBotones(Boolean estado)
+        private void opacityElementosExtras() // Terminado
+        {
+            Ataque1_ImagenArañazo.Opacity = 0;
+            Ataque2_Arena1.Opacity = 0;
+            Ataque2_Arena2.Opacity = 0;
+            Ataque2_Arena3.Opacity = 0;
+            Ataque2_Arena4.Opacity = 0;
+            Ataque3_FlechaVenenosa.Opacity = 0;
+            Ataque3_BurbujasVeneno.Opacity = 0;
+            Ataque4_Grieta1.Opacity = 0;
+            Ataque4_Grieta2.Opacity = 0;
+            Ataque4_Grieta3.Opacity = 0;
+            Ataque4_Grieta4.Opacity = 0;
+            Ataque4_Grieta5.Opacity = 0;
+        }
+        private void estadoBotones(Boolean estado) // Terminado
         {
             btn_PrimerAtaque.IsEnabled = estado;
             btn_SegundoAtaque.IsEnabled = estado;
